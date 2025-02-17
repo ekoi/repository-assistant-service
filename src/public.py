2774abb5eccff0f0ffb7c023a1a2eb258ef6b0de
ekoi <eko.indarto@dans.knaw.nl>
2025-02-17 11:03:43 +0100

    Remove unused endpoints

--- src/public.py
+++ src/public.py
@@ -23,22 +23,3 @@ def info():
     logging.info("Repository Selection and Advice Service")
     logging.debug("info")
     return project_details
-
-@router.get('/error')
-def error():
-    logging.error("Repository Selection and Advice Service")
-    logging.debug("error")
-    return HTTPException(status_code=500, detail="Internal Server Error")
-
-
-@router.get('/error2')
-def error2():
-    logging.error("------Repository Selection and Advice Service-----")
-    x = 1 / 0
-    logging.error("This will not be logged")
-    return HTTPException(status_code=500, detail="Internal Server Error")
-
-@router.get("/error_test")
-async def error_test(response: Response):
-    logging.error("got error!!!!")
-    raise ValueError("value error")
